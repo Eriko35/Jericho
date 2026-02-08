2271f3fed228b7bc17fe20e0985738850548a305
Eriko35 <jerichogolfogaming@gmail.com>
2026-02-08 18:15:09 +0800

    Changed the whole shit

--- js/firebase.js
+++ js/firebase.js
@@ -1,94 +1,66 @@
- // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js";
-  import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
-  import {getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDQ_poDvhiZFPmFpPFeEnOku1cGcNxKRRM",
-    authDomain: "kamuseo-dadf9.firebaseapp.com",
-    projectId: "kamuseo-dadf9",
-    storageBucket: "kamuseo-dadf9.firebasestorage.app",
-    messagingSenderId: "604608096712",
-    appId: "1:604608096712:web:21ecc54df78b2844b0f8fd",
-    measurementId: "G-ELWE92RRGY"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDQ_poDvhiZFPmFpPFeEnOku1cGcNxKRRM",
+  authDomain: "kamuseo-dadf9.firebaseapp.com",
+  projectId: "kamuseo-dadf9",
+  storageBucket: "kamuseo-dadf9.firebasestorage.app",
+  messagingSenderId: "604608096712",
+  appId: "1:604608096712:web:21ecc54df78b2844b0f8fd"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
 
-  function showMessage(message, divId){
-    var messsageDiv=document.getElementById(divId);
-    messsageDiv.style.display='block';
-    messsageDiv.innerHTML=message;
-    messsageDiv.style.opacity=1;
-    setTimeout(function(){
-        messsageDiv.style.opacity=0;
-    },5000)
+function showMessage(msg, id) {
+  const div = document.getElementById(id);
+  div.innerText = msg;
+  div.style.display = "block";
+}
 
-  }
-  const signUp = document.getElementById('submitSignUp');
-  signUp.addEventListener('click', (event)=> {
-    event.preventDefault();
-    const email=document.getElementById('email-sp').value;
-    const fName=document.getElementById('fName-sp').value;
-    const lName=document.getElementById('lName-sp').value;
-    const password=document.getElementById('password-sp').value;
+/* SIGN UP */
+const signUpBtn = document.getElementById("submitSignUp");
+if (signUpBtn) {
+  signUpBtn.addEventListener("click", () => {
+    const email = email-sp.value;
+    const password = password-sp.value;
+    const fName = fName-sp.value;
+    const lName = lName-sp.value;
 
-    const auth=getAuth();
-    const db=getFirestore();
-
-    createUserWithEmailAndPassword(auth, email, password).then((userCredential)=> {
-        const user=userCredential.user;
-        const userData={
-            email: email,
-            firstName: fName,
-            lastName: lName,
-        };
-        showMessage('Account Created Successfully', 'signUpMessage');
-        const docRef=doc(db, "users", user.uid);
-        setDoc(docRef, userData).then(()=>{
-            window.location.href='home.html';
-        }).catch((error)=>{
-            console.error("error writing document", error);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((cred) => {
+        return setDoc(doc(db, "users", cred.user.uid), {
+          email,
+          firstName: fName,
+          lastName: lName
         });
-    }).catch((error)=> {
-        const errorCode=error.code;
-        if(errorCode=='auth/email-already-in-use') {
-            showMessage('Email Address Already Exist', 'signUpMessage')
-        } else {
-            showMessage('unable to create user', 'signUpMessage');
-        }
-    })
-  })
-     const signIn=document.getElementById('submitSignIn');
-     signIn.addEventListener('click', (event)=>{
-        event.preventDefault();
-        const email=document.getElementById('email').value;
-        const password=document.getElementById('password').value;
-        const auth=getAuth(); 
+      })
+      .then(() => {
+        window.location.href = "home.html";
+      })
+      .catch(() => {
+        showMessage("Sign up failed", "signUpMessage");
+      });
+  });
+}
 
+/* SIGN IN */
+const signInBtn = document.getElementById("submitSignIn");
+if (signInBtn) {
+  signInBtn.addEventListener("click", () => {
+    const email = email-user.value;
+    const password = password-user.value;
 
-         signInWithEmailAndPassword(auth, email,password)
-         .then((userCredential)=>{
-            showMessage('login is successful', 'signInMessage');
-            const user=userCredential.user;
-            localStorage.setItem('loggedInUserId', user.uid);
-            window.location.href='home.html';
-         })
-         .catch((error)=>{  
-            const errorCode=error.code;
-            if(errorCode==='auth/invalid-credential'){
-                showMessage('Incorrect Email or Password', 'signInMessage');
-            }
-             else{
-                showMessage('Account does not Exist', 'signInMessage');
-             }
-         })        
-           
-     })
+    signInWithEmailAndPassword(auth, email, password)
+      .then((cred) => {
+        localStorage.setItem("loggedInUserId", cred.user.uid);
+        window.location.href = "home.html";
+      })
+      .catch(() => {
+        showMessage("Invalid email or password", "signInMessage");
+      });
+  });
+}
